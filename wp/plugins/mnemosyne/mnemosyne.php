5217ee8563f5b624da7f2b0468d6a3651e0ff76c
Andy Armstrong <andy@hexten.net>
2018-09-06 16:09:56 +0100

    Abandon framework shit.

--- wp/plugins/mnemosyne/mnemosyne.php
+++ wp/plugins/mnemosyne/mnemosyne.php
@@ -30,56 +30,7 @@ if (!defined('WPINC')) {
     die();
 }
 
-/**
- * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
- */
-define('MNEMOSYNE_VERSION', '1.0.0');
+define( 'MNEMOSYNE__FILE__', __FILE__ );
+define( 'MNEMOSYNE_BASE', plugin_basename( MNEMOSYNE__FILE__ ) );
 
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-mnemosyne-activator.php
- */
-function activate_mnemosyne()
-{
-    require_once plugin_dir_path(__FILE__) .
-        'includes/class-mnemosyne-activator.php';
-    Mnemosyne_Activator::activate();
-}
 
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-mnemosyne-deactivator.php
- */
-function deactivate_mnemosyne()
-{
-    require_once plugin_dir_path(__FILE__) .
-        'includes/class-mnemosyne-deactivator.php';
-    Mnemosyne_Deactivator::deactivate();
-}
-
-register_activation_hook(__FILE__, 'activate_mnemosyne');
-register_deactivation_hook(__FILE__, 'deactivate_mnemosyne');
-
-/**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
- */
-require plugin_dir_path(__FILE__) . 'includes/class-mnemosyne.php';
-
-/**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
- */
-function run_mnemosyne()
-{
-    $plugin = new Mnemosyne();
-    $plugin->run();
-}
-run_mnemosyne();
