99d4441b31753eeac4683534b4c56ad556759270
Andy Armstrong <andy@hexten.net>
2018-09-12 12:56:41 +0100

    Minor.

--- bin/couch.js
+++ bin/couch.js
@@ -1,9 +1,9 @@
 "use strict";
 
-const PouchDB = require('pouchdb');
-PouchDB.plugin(require('pouchdb-find'));
+const PouchDB = require("pouchdb");
+PouchDB.plugin(require("pouchdb-find"));
 
-const db = new PouchDB('http://localhost:5984/mnemosyne', {
+const db = new PouchDB("http://localhost:5984/mnemosyne", {
   auth: {
     username: "mnemosyne",
     password: "f7a49ad9-a6a8-40d2-9447-8c9697a37312"
