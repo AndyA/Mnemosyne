c47d49503498573991091fce5ae5adfd874d07d4
Andy Armstrong <andy@hexten.net>
2018-12-03 15:18:59 +0000

    Make it possible to add datebbases.

--- bin/freshen.sh
+++ bin/freshen.sh
@@ -2,12 +2,23 @@
 
 out="TTH-Reports"
 
-find "$out" -not -name "*-*" -name "*.txt" | while read fn; do
-  bn="$( basename "$fn" ".txt" )"
-  echo "$bn";
+function freshen {
+  local bn=$1
+  echo "$bn"
   node bin/report.js summary "${bn}*" > "$out/${bn}-summary.txt"
   node bin/report.js detail  "${bn}*" > "$out/${bn}.txt"
+}
+
+for new in "$@"; do
+  touch "$out/$new.txt"
 done
+
+find "$out" -not -name "*-*" -name "*.txt" | while read fn; do
+  bn="$( basename "$fn" ".txt" )"
+  freshen "$bn"
+done
+
+rsync -avP TTH-Reports pringle:/Users/smoo/Dropbox/Warez/
 
 # vim:ts=2:sw=2:sts=2:et:ft=sh
 
