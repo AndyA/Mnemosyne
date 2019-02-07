3b0e9d2ede0522206721dc514d9e6bdc5da594f4
Andy Armstrong <andy@hexten.net>
2019-02-07 18:27:37 +0000

    Pluck values from structures.

---
+++ webapp/lib/js/tools/pluck.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const jp = require("jsonpath");
+const _ = require("lodash");
+
+// Create a data structure using values plucked from another
+//
+// shape is a jsonpath, array of jsonpaths or object with jsonpath strings as values
+// data is an object
+
+class Pluck {
+  static pluck(data, shape) {
+    if (_.isArray(data))
+      return data.map(i => this.pluck(i, shape));
+
+    if (_.isString(shape))
+      return jp.value(data, shape);
+
+    if (_.isArray(shape))
+      return shape.map(path => this.pluck(data, path));
+
+    if (_.isObject(shape)) {
+      const keys = Object.keys(shape);
+      let out = {};
+      for (const key of keys)
+        out[key] = this.pluck(data, shape[key]);
+      return out;
+    }
+
+    throw new Error("Bad shape");
+  }
+
+  static pluckValues(obj, shape) {
+    const keys = Object.keys(obj);
+    let out = {};
+    for (const key of keys)
+      out[key] = this.pluck(obj[key], shape);
+    return out;
+  }
+}
+
+module.exports = Pluck;
