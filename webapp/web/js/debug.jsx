654d05bd1da0a035a7f8370475665cfc64cb2fe9
Andy Armstrong <andy@hexten.net>
2019-02-13 21:04:17 +0000

    Rename ReactJSON. Rename ReactJSON

--- webapp/web/js/debug.jsx
+++ webapp/web/js/debug.jsx
@@ -4,14 +4,14 @@ console.log("Debug mode enabled");
 
 const React = require("react");
 const ReactDOM = require("react-dom");
-const ReactJson = require("react-json-view").default;
+const ReactJSON = require("react-json-view").default;
 
 class Debugger extends React.Component {
   render() {
     return (
       <div>
         <h3>Developer Stuff</h3>
-        <ReactJson src={STASH} name="this" theme="monokai" collapsed="true" />
+        <ReactJSON src={STASH} name="this" theme="monokai" collapsed="true" />
       </div>
     );
   }
