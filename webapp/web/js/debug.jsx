fd6a1c6c2379e41bdd18969b77940d1a0a448edf
Andy Armstrong <andy@hexten.net>
2019-02-26 15:35:15 +0000

    Only mount React when page is ready.

--- webapp/web/js/debug.jsx
+++ webapp/web/js/debug.jsx
@@ -5,25 +5,26 @@ console.log("Debug mode enabled");
 const React = require("react");
 const ReactDOM = require("react-dom");
 const ReactJSON = require("react-json-view").default;
+const $ = require("jquery");
 
 class Debugger extends React.Component {
   render() {
     return (
       <div>
         <h3>Developer Stuff</h3>
-        <ReactJSON 
-          src={STASH} 
-          name="this" 
-          theme="monokai" 
-          collapsed="true" 
-          collapseStringsAfterLength={100}
-        />
+        <ReactJSON src={ STASH }
+          name="this"
+          theme="monokai"
+          collapsed="true"
+          collapseStringsAfterLength={ 100 } />
       </div>
-    );
+      );
   }
 }
 
-ReactDOM.render(
-  <Debugger />,
-  document.getElementById("debug")
-);
+$(() => {
+  ReactDOM.render(
+    <Debugger />,
+    document.getElementById("debug")
+  );
+});
