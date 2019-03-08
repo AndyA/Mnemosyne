430928d606fd12d28f0d5da8a34392a1ab4c6256
Andy Armstrong <andy@hexten.net>
2019-03-08 17:43:08 +0000

    DocPipe.

---
+++ test/unit/docpipe/docpipe.js
@@ -0,0 +1,218 @@
+"use strict";
+
+const chai = require("chai");
+const expect = chai.expect;
+const stream = require("stream");
+const Promise = require("bluebird");
+
+require("../../../webapp/use.js");
+
+const DocPipe = require("lib/js/docpipe");
+
+class Broadcasts {
+  process(doc, ctx) {
+    if (doc.kind === "broadcast")
+      return ctx.save(doc);
+  }
+}
+
+class Episodes {
+  process(doc, ctx) {
+    if (doc.kind === "episode")
+      ctx.save();
+  }
+}
+
+const docs = [
+  {
+    _id: 1,
+    kind: "broadcast",
+    episode: 2,
+    service: 4
+  }, {
+    _id: 2,
+    kind: "episode"
+  }, {
+    _id: 3,
+    kind: "broadcast",
+    episode: 2,
+    service: 4
+  }, {
+    _id: 4,
+    kind: "service"
+  }
+];
+
+describe("DocPipe", () => {
+  describe("process / processAll", () => {
+
+    it("should handle function stages", async () => {
+      const dp = new DocPipe();
+      dp.addStage((doc, ctx) => {
+        if (doc._id >= 3)
+          ctx.save();
+      });
+      const got = await dp.processAll(docs);
+      const want = docs.filter(d => d._id >= 3);
+      expect(got).to.deep.equal(want);
+    });
+
+    it("should drop all unsaved docs", async () => {
+      const dp = new DocPipe();
+      const got = await dp.processAll(docs);
+      expect(got).to.be.empty;
+    });
+
+    it("should keep all docs with allDirty = true", async () => {
+      const dp = new DocPipe({
+        allDirty: true
+      });
+      const got = await dp.processAll(docs);
+      expect(got).to.deep.equal(docs);
+    });
+
+    it("should keep all saved docs", async () => {
+      const dp = new DocPipe();
+      dp.addStage(new Broadcasts());
+      dp.addStage(new Episodes());
+      const got = await dp.processAll(docs);
+      const want = docs.filter(d => d.kind === "broadcast" || d.kind === "episode");
+      expect(got).to.deep.equal(want);
+    });
+
+    it("should respect priority", async () => {
+      const dp = new DocPipe();
+      dp
+        .addStage((doc, ctx) => {
+          doc.log.push(3); ctx.save();
+        }, 30)
+        .addStage((doc, ctx) => {
+          doc.log = [1]; ctx.save();
+        }, -10)
+        .addStage((doc, ctx) => {
+          doc.log.push(2); ctx.save();
+        }, 20)
+        .addStage((doc, ctx) => {
+          doc.log.push(4); ctx.save();
+        }, 30)
+        .addStage((doc, ctx) => ctx.save(Object.assign({}, doc)), -1000);
+
+      const got = await dp.processAll(docs);
+      expect(got).to.not.deep.equal(docs);
+
+      const want = docs.map(d => Object.assign({ }, d, {
+        log: [1, 2, 3, 4]
+      }));
+
+      expect(got).to.deep.equal(want);
+    });
+
+    it("should drop skipped docs", async () => {
+      const dp = new DocPipe();
+      dp.addStage((doc, ctx) => {
+        if (doc._id >= 3)
+          ctx.save();
+      });
+      dp.addStage((doc, ctx) => {
+        if (doc._id % 2)
+          ctx.skip();
+      });
+
+      const got = await dp.processAll(docs);
+      const want = docs.filter(d => d._id >= 3 && 0 === d._id % 2);
+      expect(got).to.deep.equal(want);
+    });
+  });
+
+  describe("Transform stream", () => {
+
+    class TestSource extends stream.Readable {
+      constructor(data) {
+        super({
+          objectMode: true
+        });
+        this.data = data.slice(0);
+      }
+
+      _read(size) {
+        if (!this.data.length) {
+          this.push(null);
+          return;
+        }
+        while (this.data.length) {
+          if (!this.push(this.data.shift()))
+            break;
+        }
+      }
+    }
+
+    function drainStream(rs) {
+      return new Promise((resolve, reject) => {
+        let docs = [];
+        rs.on("data", doc => docs.push(doc)).on("end", () => resolve(docs));
+      });
+    }
+
+    describe("TestSource", () => {
+      it("should pass all data", async () => {
+        const ts = new TestSource(docs);
+        const got = await drainStream(ts);
+        expect(got).to.deep.equal(docs);
+      });
+    });
+
+    describe("DocPipe", () => {
+
+      it("should pass all dirty docs", async () => {
+        const ts = new TestSource(docs);
+        const dp = new DocPipe({
+          allDirty: true
+        });
+        const got = await drainStream(ts.pipe(dp));
+        expect(got).to.deep.equal(docs);
+      });
+
+      it("should allow promises", async () => {
+        const ts = new TestSource(docs);
+        const dp = new DocPipe();
+
+        dp.addStage((doc, ctx) => {
+          if (doc.kind === "episode")
+            return Promise.delay(10).then(() => {
+              ctx.save(Object.assign(doc, {
+                name: "An episode"
+              }))
+            });
+        });
+
+        dp.addStage((doc, ctx) => {
+          if (doc.kind === "service")
+            return Promise.delay(5).then(() => {
+              ctx.save(Object.assign(doc, {
+                name: "A service"
+              }))
+            });
+        });
+
+        const got = await drainStream(ts.pipe(dp));
+        const want = [
+          {
+            _id: 2,
+            kind: 'episode',
+            name: 'An episode'
+          },
+          {
+            _id: 4,
+            kind: 'service',
+            name: 'A service'
+          }
+        ];
+
+        expect(got).to.deep.equal(want);
+
+      });
+
+    });
+
+  });
+});
---
+++ webapp/lib/js/docpipe/index.js
@@ -0,0 +1,97 @@
+"use strict";
+
+const stream = require("stream");
+const _ = require("lodash");
+
+class Context {
+  constructor(doc) {
+    this.doc = doc;
+    this.dirty = false;
+    this.skipped = false;
+  }
+
+  save(doc) {
+    this.dirty = true;
+    return doc || this.doc;
+  }
+
+  skip() {
+    this.skipped = true;
+  }
+}
+
+class DocPipe extends stream.Transform {
+  constructor(options) {
+
+    const opt = Object.assign({}, {
+      highWaterMark: 256,
+      allDirty: false
+    }, options || {}, {
+      objectMode: true
+    })
+
+    super(opt);
+    this.opt = opt;
+    this.seq = 0;
+    this._stages = [];
+
+    if (opt.allDirty)
+      this.addStage((doc, ctx) => ctx.save());
+  }
+
+  _checkStage(stage) {
+    if (_.isFunction(stage)) return {
+        process: stage
+      };
+    if (!stage.process)
+      throw new Error("Stage needs a process method");
+    return stage;
+  }
+
+  addStage(stage, priority = 0) {
+
+    this._stages.push({
+      stage: this._checkStage(stage),
+      priority,
+      seq: this.seq++
+    });
+
+    delete this._ordered;
+
+    return this;
+  }
+
+  get stages() {
+    return this._ordered = this._ordered || this._stages
+      .slice(0)
+      .sort((a, b) => a.priority - b.priority || a.seq - b.seq)
+      .map(s => s.stage);
+  }
+
+  async process(doc) {
+    let ctx = new Context(doc);
+    for (const stage of this.stages) {
+      const nextDoc = await stage.process(doc, ctx);
+      if (ctx.skipped) return;
+      doc = nextDoc || doc;
+    }
+    if (ctx.dirty)
+      return doc;
+  }
+
+  async processAll(docs) {
+    return Promise.all(docs.map(d => this.process(d)))
+      .then(docs => docs.filter(d => d !== undefined));;
+  }
+
+  _transform(chunk, encoding, callback) {
+    this.process(chunk).then(doc => {
+      if (doc !== undefined)
+        this.push(doc);
+      callback();
+    });
+  }
+
+}
+
+module.exports = DocPipe;
